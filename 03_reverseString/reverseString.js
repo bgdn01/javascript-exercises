3b9e7f05454237178e28ca07fa69db4307d1662b
bgdn01 <61185599+bgdn01@users.noreply.github.com>
2025-01-24 15:36:52 +0300

    Finish 03 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+// const reverseString = function (string) {
+//     let baseString = '' + string;
+//     let array = baseString.split('');
+//     let output = '';
+//     while (array.length > 0) {
+//         output += array.pop();
+//     }
+//     return output;
+// };
 
+
+// 2nd version:
+const reverseString = function (string) {
+    return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
