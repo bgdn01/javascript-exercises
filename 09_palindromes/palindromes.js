88ea97be91a237f3adaa49755983e0279d94f846
bgdn01 <61185599+bgdn01@users.noreply.github.com>
2025-02-04 17:05:16 +0300

    Finish 09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stringBase = String(string).toLowerCase();
 
+    let exceptions = /[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g;
+    stringBase = stringBase.replaceAll(exceptions, '');
+
+    let reverseString = stringBase.split('').reverse().join('');
+    return stringBase === reverseString;
 };
 
 // Do not edit below this line
