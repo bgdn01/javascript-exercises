bc3f9a1eb8b4c9bfea600efb77a536de24422f8e
bgdn01 <61185599+bgdn01@users.noreply.github.com>
2025-02-04 14:53:42 +0300

    Finish 07 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempF) {
+  let temp = ((tempF - 32) * (5 / 9)).toFixed(1);
+  temp = temp.endsWith('.0') ? temp.slice(0, temp.length - 2) : temp;
+  return Number(temp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
+  let temp = (tempC * (9 / 5) + 32).toFixed(1);
+  temp = temp.endsWith('.0') ? temp.slice(0, temp.length - 2) : temp;
+  return Number(temp);
 };
 
 // Do not edit below this line
