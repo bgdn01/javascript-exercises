18c2ccead9f31bffc2bb987f547e4ae0b2217d25
bgdn01 <61185599+bgdn01@users.noreply.github.com>
2025-02-04 15:16:01 +0300

    Finish 08 exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (numA, numB) {
+  return Number(numA) + Number(numB);
 };
 
-const subtract = function() {
-	
+const subtract = function (numA, numB) {
+  return Number(numA) - Number(numB);
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let result = 0;
+  for (let element of arr) {
+    result += element;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let result = 1;
+  for (let element of arr) {
+    result *= element;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function (numA, numB) {
+  return numA ** numB;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) return 1;
+  let factorial = 1;
+  for (let i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
