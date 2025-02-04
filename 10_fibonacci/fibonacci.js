72042c6b96736f5ea7355b72c9cb6408d1e33477
bgdn01 <61185599+bgdn01@users.noreply.github.com>
2025-02-04 18:34:45 +0300

    Finish 10 exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    let numFib = Number(num);
+    if (numFib < 0) return 'OOPS';
 
+    let fibonacciArray = [0, 1];
+    for (let i = 2; i <= numFib; i++) {
+        let newItem = (fibonacciArray[i - 1] + fibonacciArray[i - 2]);
+        fibonacciArray.push(newItem);
+    }
+    return fibonacciArray[numFib];
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+console.log(fibonacci(6));
